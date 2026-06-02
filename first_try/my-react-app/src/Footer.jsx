cd747fb4e7a689832aea121f04f9113a5dc813f2
JuanDa72 <jcruzgi@unal.edu.co>
2026-06-02 12:28:05 -0500

    first commit

---
+++ first_try/my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return (
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} My website. All rights reserved.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
